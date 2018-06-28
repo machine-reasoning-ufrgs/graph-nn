9101437acb44bca7cd6fa9e0e1b973d5bea94dd7
Marcelo Prates <marceloorp@gmail.com>
2018-06-28 08:57:56 -0300

    Updated tsp-tests.py

--- TSP/tsp-tests.py
+++ TSP/tsp-tests.py
@@ -12,6 +12,7 @@ from util import timestamp, memory_usage, dense_to_sparse, load_weights, save_we
 from tsp_utils import InstanceLoader, create_graph_metric, get_edges_mask, to_quiver
 from tsp import build_network
 from matplotlib import pyplot as plt
+from matplotlib.colors import hsv_to_rgb
 
 if __name__ == '__main__':
 
@@ -66,23 +67,54 @@ if __name__ == '__main__':
 
 			true_edges = list(zip(route,route[1:]+[route[0]]))
 			predicted_edges = [ edges[e] for e in np.nonzero(np.round(e_prob))[0] ]
-			#predicted_edges = [ edges[e] for e in e_prob.argsort()[-20:][::-1] ]
+			#predicted_edges = [ edges[e] for e in e_prob.argsort()[-40:][::-1] ]
+
+			edge_index = { (i,j):e for (e,(i,j)) in enumerate(edges) }
 
 			# Draw nodes
 			plt.scatter(x=nodes[:,0], y=nodes[:,1])
 
-			# Draw predicted edges
-			for (i,j) in predicted_edges:
+			# For each node
+			visited = set()
+			i = np.random.randint(n)
+			while len(visited) < n:
+				# Get the most probable connection
+				j = np.array([ e_prob[edge_index[(i,j)]] if (Ma[i,j] == 1 and i != j and j not in visited) else 0 for j in range(n) ]).argsort()[-1]
+				# Get coords
 				x1,x2 = nodes[i,0],nodes[j,0]
 				y1,y2 = nodes[i,1],nodes[j,1]
-				plt.plot([x1,x2],[y1,y2], 'r--', linewidth=0.5)
+				# Draw edge
+				if (i,j) in true_edges:
+					plt.plot([x1,x2],[y1,y2], 'b', linewidth=1)
+				else:
+					plt.plot([x1,x2],[y1,y2], 'r--', linewidth=1)
+				#end
+				# Add to visited
+				i = j 
+				visited.add(i)
 			#end
 
-			# Draw true edges
+			#for e,(i,j) in enumerate(edges):
+			#	if e_prob[e] < 0.5:
+			#		continue
+			#	#end
+			#	x1,x2 = nodes[i,0],nodes[j,0]
+			#	y1,y2 = nodes[i,1],nodes[j,1]
+			#	plt.plot([x1,x2],[y1,y2], color=hsv_to_rgb((1,1,(e_prob[e]-0.5)/0.5)), linewidth=e_prob[e])
+			##end
+
+			### Draw predicted edges
+			#for (i,j) in predicted_edges:
+			#	x1,x2 = nodes[i,0],nodes[j,0]
+			#	y1,y2 = nodes[i,1],nodes[j,1]
+			#	plt.plot([x1,x2],[y1,y2], 'g--', linewidth=0.5)
+			##end
+
+			## Draw true edges
 			#for (i,j) in true_edges:
 			#	x1,x2 = nodes[i,0],nodes[j,0]
 			#	y1,y2 = nodes[i,1],nodes[j,1]
-
+#
 			#	if (i,j) in predicted_edges:
 			#		plt.plot([x1,x2],[y1,y2], 'b-')
 			#	else:
