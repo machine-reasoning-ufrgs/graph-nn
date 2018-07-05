5241df0f0378e82db1c42bc37f65502686074df5
dragonavelar <dragonavelar@gmail.com>
2018-07-05 13:11:49 -0300

    Partial commit for testing neurosat with a different number of timesteps

--- neurosat/instance_loader.py
+++ neurosat/instance_loader.py
@@ -1,43 +1,53 @@
-
 import os
 import random
 from cnf import CNF, BatchCNF, create_batchCNF
 
 class InstanceLoader(object):
 
-	def __init__(self,path):
-		self.path = path
+  def __init__(self,path):
+    assert os.path.isdir( path ), "Path is not a directory. Path {}".format( path ) 
+    if path[-1] == "/":
+      path = path[0:-1]
+    #end if
+    folders = [path]
+    self.filenames = []
+    while len(folders)>0:
+      newfolders = []
+      for folder in folders:
+        newfolders += [ f.path for f in os.scandir(folder) if f.is_dir() ]
+        self.filenames += [ f.path for f in os.scandir(folder) if f.is_file() and f.path.endswith(".cnf") ]
+      #end for
+      folders = newfolders
+    #end while
+    self.reset()
+  #end
 
-		self.filenames = [ 'instances/sat/'+x for x in os.listdir(path + '/sat')] + [ 'instances/unsat/'+x for x in os.listdir(path + '/unsat') ]
-		self.reset()
-	#end
+  def get_instances(self, n_instances):
+    for i in range(n_instances):
+      yield CNF.read_dimacs(self.filenames[self.index])
+      self.index += 1
+    #end
+  #end
 
-	def get_instances(self, n_instances):
-		for i in range(n_instances):
-			yield CNF.read_dimacs(self.filenames[self.index])
-			self.index += 1
-		#end
-	#end
+  def get_batches(self, batch_size):
+    for i in range( len(self.filenames) // batch_size ):
+      yield create_batchCNF(self.get_instances(batch_size))
+    #end
+  #end
 
-	def get_batches(self, batch_size):
-		for i in range( len(self.filenames) // batch_size ):
-			yield create_batchCNF(self.get_instances(batch_size))
-		#end
-	#end
-
-	def reset(self):
-		random.shuffle( self.filenames )
-		self.index = 0
-	#end
+  def reset(self):
+    random.shuffle( self.filenames )
+    self.index = 0
+  #end
 #end
 
 if __name__ == '__main__':
 
-	instance_loader = InstanceLoader("instances")
+  instance_loader = InstanceLoader("instances")
 
-	batches = instance_loader.get_batches(32)
+  batches = instance_loader.get_batches(32)
 
-	for batch in batches:
-		print( batch.get_sparse_matrix() )
-	#end
+  for batch in batches:
+    print( batch.get_sparse_matrix() )
+  #end
 #end
